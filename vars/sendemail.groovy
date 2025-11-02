5e8faad8a9837ccb77a3b3d650cdaed0a62c1050
Manoj0919 <kummaramanojkumarr@gmail.com>
2025-11-02 20:52:48 +0530

    m

--- vars/sendemail.groovy
+++ vars/sendemail.groovy
@@ -6,17 +6,18 @@ def sendEmail(String subject, String body, String recipient) {
         mimeType: 'text/html'
     )
 }
-def success = success {
-                sendEmail(
-                    "${env.JOB_NAME} - ${env.BUILD_NUMBER} - Build success.",
-                    "Build sucess. Please check the console output at ${env.BUILD_URL}", 'devopsmaonj1909@gmail.com'
-                    )
-                }
-        
-def failure = failure{
-                sendEmail(
-                    "${env.JOB_NAME} - ${env.BUILD_NUMBER} - Build FAILED to copy the files",
-                    "Build FAILED. Files are copied to nignix. Please check the console output at ${env.BUILD_URL}",'devopsmanu1909@gmail.com'
-                    )
-                 }
+post{
+    success {
+             sendEmail(
+                "${env.JOB_NAME} - ${env.BUILD_NUMBER} - Build success.",
+                "Build sucess. Please check the console output at ${env.BUILD_URL}", 'devopsmaonj1909@gmail.com'
+                 )
+    }
     
+        failure{
+            sendEmail(
+                "${env.JOB_NAME} - ${env.BUILD_NUMBER} - Build FAILED to copy the files",
+                "Build FAILED. Files are copied to nignix. Please check the console output at ${env.BUILD_URL}",'devopsmanu1909@gmail.com'
+                 )
+    }
+}
