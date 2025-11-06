13469e146b0f9443541b4d17ab9c52dba71c43bc
Manoj0919 <kummaramanojkumarr@gmail.com>
2025-11-06 10:23:56 +0530

    mh

--- vars/getbranchname.groovy
+++ vars/getbranchname.groovy
@@ -1,4 +1,4 @@
 // vars/getBranchName.groovy
-def call(def branchname) {
-   ${branchname} = sh(script: "git name-rev --name-only HEAD", returnStdout: true).trim()
+def call() {
+    sh(script: "git name-rev --name-only HEAD", returnStdout: true).trim()
 }
